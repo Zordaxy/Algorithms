7a640cac252493ee8102c9224ed609d1a7b1a293
Alex Vovchuk <avovchuk@amcbridge.com>
2018-11-18 17:59:47 +0200

    Implement HeapSort for JS

--- src/algs4/JavaScriptSorting/Sort.js
+++ src/algs4/JavaScriptSorting/Sort.js
@@ -162,11 +162,36 @@ console.log("QuickSort    ", array);
 
 class HeapSort {
     static sort(array) {
+        array.push(array[0]);
+        let N = array.length - 1;
+        for (let k = Math.floor(N / 2); k >= 1; k--) {
+            this._sink(array, k, N);
+        }
 
+        while (N > 1) {
+            this._swap(array, 1, N--);
+            this._sink(array, 1, N);
+        }
 
+        array.shift();
         return array;
+    }
+
+    static _sink(array, index, end) {
+        let j;
+        while ((j = index * 2) <= end) {
+            if (j + 1 <= end && array[j] < array[j + 1]) j++;
+            if (array[j] > array[index]) this._swap(array, index, j);
+            index = j;
+        }
+    }
+
+    static _swap(array, a, b) {
+        let temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
 array = initialArray.slice();
 result = HeapSort.sort(array);
-console.log("HeapSort     ", array, "Not implemented");
+console.log("HeapSort     ", result);
